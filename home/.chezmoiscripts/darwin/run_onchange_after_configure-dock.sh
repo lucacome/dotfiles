5a6d2741d39aa13da6880d12bc4ac4181be0c5ea
Luca Comellini <luca.com@gmail.com>
2026-02-19 21:29:37 -0800

    true

--- home/.chezmoiscripts/darwin/run_onchange_after_configure-dock.sh
+++ home/.chezmoiscripts/darwin/run_onchange_after_configure-dock.sh
@@ -25,8 +25,8 @@ for label in "${remove_labels[@]}"; do
 	dockutil --no-restart --remove "${label}" || true
 done
 
-dockutil --add /Applications/Signal.app --after 'Messages'
-dockutil --add /Applications/Ghostty.app --after 'System Settings'
-dockutil --add /Applications/zoom.us.app --after 'Ghostty'
-dockutil --add /System/Applications/Utilities/Activity%20Monitor.app --after 'zoom.us'
-dockutil --add /Applications/Visual%20Studio%20Code%20-%20Insiders.app --after 'iPhone Mirroring'
+dockutil --add /Applications/Signal.app --after 'Messages' || true
+dockutil --add /Applications/Ghostty.app --after 'System Settings' || true
+dockutil --add /Applications/zoom.us.app --after 'Ghostty' || true
+dockutil --add /System/Applications/Utilities/Activity%20Monitor.app --after 'zoom.us' || true
+dockutil --add /Applications/Visual%20Studio%20Code%20-%20Insiders.app --after 'iPhone Mirroring' || true
