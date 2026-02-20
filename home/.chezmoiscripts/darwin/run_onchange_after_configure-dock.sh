902c624646486a43221eb6dd904058ecff3e6ce9
Luca Comellini <luca.com@gmail.com>
2026-02-19 21:28:32 -0800

    dock

--- home/.chezmoiscripts/darwin/run_onchange_after_configure-dock.sh
+++ home/.chezmoiscripts/darwin/run_onchange_after_configure-dock.sh
@@ -24,3 +24,9 @@ declare -a remove_labels=(
 for label in "${remove_labels[@]}"; do
 	dockutil --no-restart --remove "${label}" || true
 done
+
+dockutil --add /Applications/Signal.app --after 'Messages'
+dockutil --add /Applications/Ghostty.app --after 'System Settings'
+dockutil --add /Applications/zoom.us.app --after 'Ghostty'
+dockutil --add /System/Applications/Utilities/Activity%20Monitor.app --after 'zoom.us'
+dockutil --add /Applications/Visual%20Studio%20Code%20-%20Insiders.app --after 'iPhone Mirroring'
