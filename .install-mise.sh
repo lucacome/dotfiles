4a60908dcd936b9f59b8c0850ce32f0ec8a4cd31
Luca Comellini <luca.com@gmail.com>
2026-03-10 19:11:17 -0700

    mise

---
+++ .install-mise.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# exit immediately if mise is already in $PATH
+type mise >/dev/null 2>&1 && exit
+
+case "$(uname -s)" in
+Darwin)
+    brew install mise
+    ;;
+Linux)
+	apt-get update -y && apt-get install -y curl
+	install -dm 755 /etc/apt/keyrings
+	curl -fSs https://mise.jdx.dev/gpg-key.pub | tee /etc/apt/keyrings/mise-archive-keyring.asc 1> /dev/null
+	echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.asc] https://mise.jdx.dev/deb stable main" | tee /etc/apt/sources.list.d/mise.list
+	apt-get update -y
+	apt-get install -y mise
+	;;
+*)
+    echo "unsupported OS"
+    exit 1
+    ;;
+esac
+
